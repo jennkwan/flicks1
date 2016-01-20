63ba4b5e0b02e51cfb56e1239e97400fcd714f39
Jennifer Kwan <jenniferkwan@miles.local>
2016-01-19 16:51:22 -0800

    complete video tutorial

---
+++ movieViewer/MovieCell.swift
@@ -0,0 +1,20 @@
+//
+//  MovieCell.swift
+//  movieViewer
+//
+//  Created by Jennifer Kwan on 1/18/16.
+//  Copyright © 2016 Jennifer Kwan. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var overviewLabel: UILabel!
+    @IBOutlet weak var posterImage: UIImageView!
+
+    @IBOutlet weak var posterView: UIView!
+
+}
